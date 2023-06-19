735dc5daf4b0f0d7e0903715e253c17d549adef9
SAM <34885543+sansernsam@users.noreply.github.com>
2023-06-20 01:02:25 +0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Hello-gastby`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
